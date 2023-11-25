a93e180c85ae336d364e1995b1f0107fe71f74e4
Sam Llanwarne <errerr46@gmail.com>
2023-11-25 20:27:56 +0000

    change to button, wrong github repo

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import NavBar from '../components/NavBar';
-import { db } from '../services/firebaseConfig'; // Import Firestore database
+import { auth, db } from '../services/firebaseConfig'; // Import Firestore database
 import { collection, onSnapshot } from 'firebase/firestore'; // Import Firestore functions
 
 function Home() {
@@ -12,8 +12,9 @@ function Home() {
 
     // Fetching the current activity from Firestore
     useEffect(() => {
+        const user = auth.currentUser;
         // Assuming 'USER_ID' is replaced with the actual user ID
-        const userCollectionRef = collection(db, 'users', 'USER_ID', 'currentActivity');
+        const userCollectionRef = collection(db, 'users', user.uid, 'currentActivity');
         
         const unsubscribe = onSnapshot(userCollectionRef, snapshot => {
             if (!snapshot.empty) {
@@ -58,7 +59,7 @@ function Home() {
             <Button 
                 onClick={handleActivityClick} 
                 className="bigButton" 
-                label={currentActivity ? "Finish Activity" : "Log Activity"} 
+                label={currentActivity ? "Finish Activity" : "Start Activity"} 
             />
             <div>{currentActivity ? `Current Activity: ${currentActivity.name}` : "Just Chilling"}</div>
             <div>Timer: {timer}</div>
